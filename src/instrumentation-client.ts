fc070296225bd9049c10110e936fbcd0700b6161
Liviu <dev.workspacehub@gmail.com>
2026-08-18 16:30:14 +0300

    fix: enable page view capturing in PostHog initialization

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -4,7 +4,8 @@ if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://eu.i.posthog.com",
     autocapture: false,
-    capture_pageview: false,
+    capture_pageview: true,
+    cookieless_mode: "always",
     disable_session_recording: true,
     capture_exceptions: true,
   });
